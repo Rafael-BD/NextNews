c53b3b2bbadaaab8b24f8f01bc358a3f87ea6956
Rafael Bettinidias <rafael8885326@gmail.com>
2023-10-19 21:51:48 -0300

    arquivos e estrutura inicial criada

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+    return (
+        <div>
+            <h1>Welcome to my basic page!</h1>
+            <p>This is a simple example of a Next.js page with TypeScript.</p>
+        </div>
+    );
+};
+
+export default HomePage;
